4f01cdae8912543855f36c96a251349eefd248b4
Carmen <33059@ma-web.nl>
2022-05-30 12:01:00 +0200

    data base ontwerp

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+require 'functions.php';
+$connection = dbConnect();
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Databse</title>
+</head>
+
+<body>
+
+    <article class="card">
+        <figure class="card__figure">
+            <img src="/img/ring.JPG" alt="dure ring">
+        </figure>
+
+        <section class="card__body">
+
+            <header>
+                <h2 class="card__heading">Dure ring</h2>
+            </header>
+            <em>400</em>
+            <p class="card__p">Hele dure ring</p>
+            
+        </section>
+   </article>
+</body>
+
+</html>
